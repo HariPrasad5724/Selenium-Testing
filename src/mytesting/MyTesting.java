c63b46ce5b8a7a5653eb8711eaf84c5a3e4a3191
HariPrasad5724 <hariprasad572415@gmail.com>
2021-04-26 20:26:08 +0530

    Added Stock, Bill and Account Home

--- src/mytesting/MyTesting.java
+++ src/mytesting/MyTesting.java
@@ -18,6 +18,20 @@ public class MyTesting {
         driver.quit();
     }
     
+    void completeTest() throws InterruptedException{
+        System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+        String baseUrl = "http://localhost:9999/Stock%20Maintenance/servlets/Login";
+        driver.get(baseUrl);
+        driver.manage().window().maximize();
+        driver.findElement(By.id("user")).sendKeys("Hari Prasadh");
+        driver.findElement(By.id("pwd")).sendKeys("admin");
+        Thread.sleep(3000);
+        driver.findElement ((By.id ("login"))).click ();
+        Thread.sleep(3000);
+        
+    }
+    
     void StockHome() throws InterruptedException{
         System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
@@ -26,10 +40,10 @@ public class MyTesting {
         driver.manage().window().maximize();
         driver.findElement ((By.id ("insertstock"))).click ();
         driver.findElement(By.id("ftype")).sendKeys("Airtex");
-        driver.findElement(By.id("fcolor")).sendKeys("White");
-        driver.findElement(By.id("fgsm")).sendKeys("200");
-        driver.findElement(By.id("fcost")).sendKeys("250");
-        driver.findElement(By.id("fweight")).sendKeys("80");
+        driver.findElement(By.id("fcolor")).sendKeys("BLUE");
+        driver.findElement(By.id("fgsm")).sendKeys("250");
+        driver.findElement(By.id("fcost")).sendKeys("200");
+        driver.findElement(By.id("fweight")).sendKeys("120");
         Thread.sleep(3000);
         driver.findElement ((By.id ("addfabric"))).click ();
         Thread.sleep(3000);
@@ -117,11 +131,11 @@ public class MyTesting {
         driver.manage().window().maximize();
         driver.findElement ((By.id ("salesinsert"))).click ();
         Thread.sleep(3000);
-        driver.findElement(By.id("scname")).sendKeys("HARRIS");
-        driver.findElement(By.id("sbillno")).sendKeys("1006");
-        driver.findElement(By.id("sweight")).sendKeys("70");
-        driver.findElement(By.id("samount")).sendKeys("10000");
-        driver.findElement(By.id("sday")).sendKeys("20");
+        driver.findElement(By.id("scname")).sendKeys("JAGA");
+        driver.findElement(By.id("sbillno")).sendKeys("1009");
+        driver.findElement(By.id("sweight")).sendKeys("75");
+        driver.findElement(By.id("samount")).sendKeys("11000");
+        driver.findElement(By.id("sday")).sendKeys("23");
         driver.findElement(By.id("smonth")).sendKeys("JAN");
         driver.findElement(By.id("syear")).sendKeys("2021");
         Thread.sleep(3000);
@@ -131,11 +145,11 @@ public class MyTesting {
         Thread.sleep(3000);
         driver.findElement ((By.id ("purchaseinsert"))).click ();
         Thread.sleep(3000);
-        driver.findElement(By.id("pcname")).sendKeys("ULLAS");
-        driver.findElement(By.id("pbillno")).sendKeys("2008");
-        driver.findElement(By.id("pweight")).sendKeys("40");
-        driver.findElement(By.id("pamount")).sendKeys("2500");
-        driver.findElement(By.id("pday")).sendKeys("28");
+        driver.findElement(By.id("pcname")).sendKeys("RUSHWANTH");
+        driver.findElement(By.id("pbillno")).sendKeys("2009");
+        driver.findElement(By.id("pweight")).sendKeys("70");
+        driver.findElement(By.id("pamount")).sendKeys("2800");
+        driver.findElement(By.id("pday")).sendKeys("21");
         driver.findElement(By.id("pmonth")).sendKeys("FEB");
         driver.findElement(By.id("pyear")).sendKeys("2021");
         Thread.sleep(3000);
@@ -154,12 +168,33 @@ public class MyTesting {
         Thread.sleep(3000);
         driver.findElement((By.id("searchviadate"))).click();
         Thread.sleep(3000);
-         driver.findElement(By.id("stype")).sendKeys("sales");
+        driver.findElement(By.id("stype")).sendKeys("sales");
         driver.findElement(By.id("sday")).sendKeys("9");
         driver.findElement(By.id("smonth")).sendKeys("JAN");
         driver.findElement(By.id("syear")).sendKeys("2020");
         Thread.sleep(3000);
         driver.findElement ((By.id ("searchviadate"))).click ();
+        Thread.sleep(3000);
+        driver.findElement ((By.id ("home"))).click ();
+        Thread.sleep(3000);
+        driver.quit();
+    }
+    void AccountsHome() throws InterruptedException{
+        System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+        String baseUrl = "http://localhost:9999/Stock%20Maintenance/servlets/Accounts/Home.jsp?";
+        driver.get(baseUrl);
+        driver.manage().window().maximize();
+        driver.findElement ((By.id ("monthlyprofit"))).click ();
+        Thread.sleep(3000);
+        driver.findElement(By.id("a_month")).sendKeys("JAN");
+        driver.findElement(By.id("a_year")).sendKeys("2021");
+        Thread.sleep(3000);
+        driver.findElement ((By.id ("monthprofit"))).click ();
+        Thread.sleep(3000);
+        driver.findElement ((By.id ("home"))).click ();
+        Thread.sleep(3000);
+        driver.findElement ((By.id ("profitcheck"))).click ();
         Thread.sleep(3000);
         driver.findElement ((By.id ("home"))).click ();
         Thread.sleep(3000);
@@ -185,6 +220,7 @@ public class MyTesting {
         Thread.sleep(3000);
         driver.quit();
     }
+     
       void addPurchaseBill()throws InterruptedException{
         System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
@@ -244,13 +280,14 @@ public class MyTesting {
         MyTesting m=new MyTesting();
        // m.addFabric();
        // m.removeFabric();
-          m.searchFabric();
-       // m.Login();
-       // m.StockHome();
-       // m.BillsHome();
+       //  m.searchFabric();
+       //m.Login();
+       //m.StockHome();
+       //m.BillsHome();
        // m.addPurchaseBill();
        // m.addSalesBill();
        // m.searchBillno();
        // m.searchViaDate();
+       //m.AccountsHome();
     }
 }
